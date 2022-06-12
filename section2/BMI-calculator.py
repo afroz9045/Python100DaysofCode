2de98ca079adade6e5e336cd6e5990a684b0c75b
Afroz <afroz9045@gmail.com>
2022-06-12 22:32:17 +0530

    BMI code updated

--- section2/BMI-calculator.py
+++ section2/BMI-calculator.py
@@ -1,8 +1,17 @@
 # General formal for BMI calculator: weight(kg)/height*2(m)
 
+
+
 weight = int(input("Enter your weight in KG: \n"))
-height = float(input("Enter your height in M: \n"))
+height = int(input("Enter your height in CM: \n"))
 
-BMI = weight/(height**2)
-
-print(f"Your BMI is : {int(BMI)}")
+BMI = weight/(height*2/100);
+if BMI>30:
+    print(f"You are obese! as your BMI is: {int(BMI)}");
+    
+elif BMI>25 and BMI<=30:
+    print(f"You are overweight as your BMI is: {int(BMI)}");
+elif BMI>=18.5 and BMI<=25:
+    print(f"You are normal as your BMI is: {int(BMI)}");
+else:
+    print(f"You are underweight as your BMI is: {int(BMI)}");
